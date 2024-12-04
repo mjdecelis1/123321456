a0484c853bc3259f4a38db4a8f61586a137286ca
mjdecelis1 <micodecelis123@gmail.com>
2024-12-04 23:20:57 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,28 @@
 function backCategories() {
     window.location.href = 'categories.html';  
 }
-function goCategories() {
+function goCategories(){
     window.location.href = 'categories.html';
 }
+
+function goMain(){
+    window.location.href = 'Main.html';  
+
+
+} function goAppetizer(){
+    window.location.href = 'Appetizer.html';  
+
+} function goSalad(){
+    window.location.href = 'Salads.html';  
+
+}  function goDessert(){
+    window.location.href = 'Desserts.html';  
+
+} function goSoup(){
+    window.location.href = 'Soups.html';  
+
+}
+
 
 var categories = [
     document.getElementById('food1Container'),
@@ -16,32 +35,31 @@ var defaultSize = { width: '90%', height: '160px' };
 var expandedSize = { width: '90%', height: '140vh' };
 
 categories.forEach((container) => {
-    let isExpanded;
+    let isExpanded = false; 
     const arrow = container.querySelector('.arrowDown'); 
     const dish = container.querySelector('.dishesDetails'); 
 
-
-    container.addEventListener('click', (event) => {
-      
-        if (event.target === arrow) {
-            if (isExpanded) {
-                isExpanded = false;
-                container.style.width = defaultSize.width;
-                container.style.height = defaultSize.height;
-                arrow.style.transform = 'rotate(0deg)';  
-                dish.style.display = 'none'; 
-                container.classList.remove('no-hover');  
-            } else {
-                isExpanded = true;
-                container.style.width = expandedSize.width;
-                container.style.height = expandedSize.height;
-                arrow.style.transform = 'rotate(180deg)'; 
-                dish.style.display = 'block'; 
-                container.classList.add('no-hover');
-            }
+    container.addEventListener('click', () => {
+        if (isExpanded) {
+            
+            container.style.width = defaultSize.width;
+            container.style.height = defaultSize.height;
+            arrow.style.transform = 'rotate(0deg)';  
+            dish.style.display = 'none'; 
+            container.classList.remove('no-hover');  
+        } else {
+            
+            container.style.width = expandedSize.width;
+            container.style.height = expandedSize.height;
+            arrow.style.transform = 'rotate(180deg)'; 
+            dish.style.display = 'block'; 
+            container.classList.add('no-hover');
         }
+
+        isExpanded = !isExpanded;
     });
 });
+
 function toggleMenu() {
     const menu = document.getElementById("menuDropdown");
     menu.classList.toggle("show");
